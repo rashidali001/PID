b552e692f3e5ff67350a079cc9f4df3445891b2e
rashidali001 <rashidestonian@gmai.com>
2022-08-31 14:49:08 -0700

    PID repo commit

---
+++ getline.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int ac, char **av)
+{
+	char *buffer;
+	size_t bufferSize = 32;
+	size_t characters;
+	
+	buffer = (char *)malloc(sizeof(char) * bufferSize);
+
+	if (buffer == NULL)
+	{
+		printf("unable to allocate memory");
+		exit(1);
+	}
+	printf("$ ");
+	characters = getline(&buffer, &bufferSize, stdin);
+	printf("%s", buffer);
+
+	return (0);
+}
+
+
